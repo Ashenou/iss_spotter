4a65cd8fde93478bff6e9321794fcdf4a9ccdc9b
Abanoub Shenouda <Abanoub Shenouda>
2021-06-03 19:56:57 -0400

    Added iis oce

---
+++ index.js
@@ -0,0 +1,42 @@
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned IP:", ip);
+  this.ip = ip;
+});
+
+fetchCoordsByIP("162.245.144.188", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates:", coordinates);
+});
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const { fetchISSFlyOverTimes } = require("./iss");
+
+const exampleCoords = { latitude: "49.27670", longitude: "-123.13000" };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned flyover times:", passTimes);
+});
